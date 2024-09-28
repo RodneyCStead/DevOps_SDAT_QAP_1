de454fecdb01e48c586a662cc3f32c302640dba5
RodneyCStead <RodCStead@gmail.com>
2024-09-28 00:35:03 -0230

    made a test for the constructor to test if it initializes correct

--- src/test/java/com/keyin/VideoGameTest.java
+++ src/test/java/com/keyin/VideoGameTest.java
@@ -1,5 +1,17 @@
 package com.keyin;
 
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
 public class VideoGameTest {
 
+    @Test
+    public void testVideoGameConstructor() {
+        VideoGame VideoGameUnderTest = new VideoGame("Guild Wars 2", "MMORPG", 10, 29.99);
+
+        Assertions.assertEquals("Guild Wars 2", VideoGameUnderTest.getTitle());
+        Assertions.assertEquals("MMORPG", VideoGameUnderTest.getGenre());
+        Assertions.assertEquals(10, VideoGameUnderTest.getRating());
+        Assertions.assertEquals(29.99, VideoGameUnderTest.getPrice());
+    }
 }
