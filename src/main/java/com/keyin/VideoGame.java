568b4ed1a315f7bf1d32c8ce43323bcab313dfc5
RodneyCStead <RodCStead@gmail.com>
2024-09-28 00:23:20 -0230

    made base class of VideoGame and VideoGameTest, VideoGame class has some basics to start testing

--- src/main/java/com/keyin/VideoGame.java
+++ src/main/java/com/keyin/VideoGame.java
@@ -1,2 +1,33 @@
+package com.keyin;
 
+public class VideoGame {
 
+    private String title;
+    private String genre;
+    private int rating;
+    private double price;
+
+    public VideoGame(String title, String genre, int rating, double price) {
+        this.title = title;
+        this.genre = genre;
+        this.rating = rating;
+        this.price = price;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getGenre() {
+        return genre;
+    }
+
+    public int getRating() {
+        return rating;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+}
